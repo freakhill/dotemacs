2fce341fb7e2cf1f144bea83b3ca77df2f10492e
johan gall <gall.johan@linecorp.com>
2014-07-10 11:40:52 +0900

    merged with windows

--- init.el
+++ init.el
@@ -1,4 +1,20 @@
-(setq user-full-name "Johan Gall")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (adwaita)))
+ '(global-ede-mode t)
+ '(inhibit-startup-screen t)
+ '(semantic-mode t)
+ '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;--------------------
 ;--- for emacs 24+ --
@@ -21,10 +37,19 @@
 			nrepl
 			evil
 			god-mode
-			evil-god-state))
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+			evil-god-state
+                        everything))
+  (defun autoinstall ()
+    (dolist (p my-packages)
+      (when (not (package-installed-p p))
+        (package-install p))))
+  (condition-case nil
+      (autoinstall)
+    (error (progn
+             (package-refresh-contents)
+             (autoinstall)))))
+
+;--------------------------------------- end of autoinstall --------------------
 
 (evil-mode 1)
 ; C-z switches between modes (states in evil parlance)
@@ -38,6 +63,11 @@
 (setq inhibit-startup-message t)
 (setq inhibit-startup-echo-area-message t)
 
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode) ; me no liek
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
 ;--- Backup files
 (setq backup-directory-alist
 `((".*" . ,temporary-file-directory)))
@@ -49,11 +79,21 @@
 (setq tab-width 4)
 (setq indent-tabs-mode nil)
 
+;--- ido, recentf, lacarte, hippie-expand, ibuffer, occur
 (ido-mode t)
 (ido-everywhere t)
-(recentf-mode 1)
 
-;---
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+(require 'lacarte)
+(global-set-key (kbd "ESC M-x") 'lacarte-execute-command)
+
+(autoload 'ibuffer "ibuffer" "List buffers." t)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-l") 'goto-line)
+(global-set-key (kbd "C-o") 'occur)
+
 (global-set-key "\M- " 'hippie-expand)
 (setq hippie-expand-try-functions-list
       '(try-expand-dabbrev
@@ -91,13 +131,55 @@
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 
-; --- projectile (find files in projects)
+;--- projectile (find files in projects)
 (projectile-global-mode)
 (setq projectile-show-paths-function 'projectile-hashify-with-relative-paths)
 (global-set-key (kbd "C-p") 'projectile-find-file)
 (global-set-key (kbd "C-o") 'recentf-open-files)
 
-; --- clojure and nrepl hooks for autocompletion and my custom fun
+;--------------------------------------- all my coding from here ---------------
+
+;--------------
+;--- Haskell --
+;--------------
+
+; todo
+
+;-----------
+;--- Rust --
+;-----------
+
+; todo
+
+;------------
+;--- Ocaml --
+;------------
+
+; todo
+
+;------------
+;--- Shell --
+;------------
+
+; todo -- i don't know if there is much emacs stuff for that
+
+;-----------
+;--- JAVA --
+;-----------
+
+; for now i prefer eclipse...
+
+;-----------
+;--- Ruby --
+;-----------
+
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+;-----------
+;--- LISP --
+;-----------
+
+;--- clojure and nrepl hooks for autocompletion and my custom fun
 (defvar lisp-mode-hooks
   '(clojure-mode-hook
     clojurescript-mode-hook
@@ -116,17 +198,24 @@
   (require 'smartparens-config)
   (smartparens-strict-mode)
   (rainbow-delimiters-mode)
-  ;--- navigation
+  ;--- navigation (just merge macos and windows config, will have to
+  ; work on refining that!)
   (local-set-key (kbd "C-f") 'sp-forward-sexp)
   (local-set-key (kbd "M-f") 'sp-backward-sexp)
   (local-set-key (kbd "ESC <up>") 'sp-up-sexp)
   (local-set-key (kbd "ESC <down>") 'sp-down-sexp)
+  (define-key sp-keymap (kbd "M-<left>") 'sp-backward-sexp)
+  (define-key sp-keymap (kbd "M-<right>") 'sp-forward-sexp)
+  (define-key sp-keymap (kbd "M-<up>") 'sp-up-sexp)
+  (define-key sp-keymap (kbd "M-<down>") 'sp-down-sexp)
   ;--- manipultation
   (local-set-key (kbd "ESC C-<right>") 'sp-forward-slurp-sexp)
   (local-set-key (kbd "ESC C-<left>") 'sp-forward-barf-sexp)
   ;--- custom entry stuff
   (local-set-key (kbd "<f1>") 'insert-parentheses)
-  (local-set-key (kbd "<f2>") 'insert-quote-char))
+  (local-set-key (kbd "<f2>") 'insert-quote-char)
+  (local-set-key (kbd "C-:") 'insert-parentheses)
+  (local-set-key (kbd "C-;") 'insert-parentheses))
 
 ;--- nrepl
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
@@ -136,3 +225,45 @@
 (defun insert-quote-char ()
   (insert-char #x0027)) ; codepoint for ' <- simple quote
 
+(defun my-reindent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(setq nrepl-hide-special-buffers t)
+;(setq cider-popup-stacktraces nil)
+;(setq cider-repl-popup-stacktraces t)
+(setq nrepl-popup-stacktraces-in-repl t)
+(setq nrepl-history-file "~/nrepl-history.dat")
+
+;-------------------------------------------- os customization from here--------
+
+;-----------------------
+;--- OS customization --
+;-----------------------
+
+(defun macos-custom ()
+  )
+
+(defun windows-custom ()
+  (setq everything-use-ftp t)
+  (setq everything-host "127.0.0.1")
+  (setq everything-port 22222)
+  (setq everything-user "emacs")
+  ; that should not be on github but... oh well... please don't try to
+  ; find me t_t...
+  (setq everything-pass "I have made a ceaseless effort not to ridicule, not to bewail, not to scorn human actions, but to understand them.")
+  (global-set-key (kbd "C-x f") 'everything-find-file))
+
+(defun linux-custom ()
+  )
+
+(cond
+ ((string-match "darwin" system-configuration)
+  (message "customizing GNU Emacs for MacOS")
+  (macos-custom))
+ ((string-match "linux" system-configuration)
+  (message "customizing GNU Emacs for Linux")
+  (linux-custom))
+ ((string-match "nt6" system-configuration)
+  (message "customizing GNU Emacs for Win 7")
+  (windows-custom)))
