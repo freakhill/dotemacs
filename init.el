ddfe793ecb0ed3dc93989f4d302b98e545665937
johan gall <gall.johan@linecorp.com>
2014-09-12 15:26:34 +0900

    some improvements

--- init.el
+++ init.el
@@ -18,7 +18,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(shm-current-face ((t (:background "green"))))
+ '(shm-quarantine-face ((t (:background "yellow")))))
 
 ;;TODO!
 ;; need to automate:
