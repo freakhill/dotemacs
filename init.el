dc12037e5cb5040d52ed8e0e050bbadc55a0e1ea
johan gall <gall.johan@linecorp.com>
2014-10-31 15:37:22 +0900

    changed a binding

--- init.el
+++ init.el
@@ -394,7 +394,7 @@
   (require 'multiple-cursors)
   (global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this))
+  (global-set-key (kbd "C-c C-SPC") 'mc/mark-all-like-this))
 
 (defun my-windmove ()
   (when (fboundp 'windmove-default-keybindings)
