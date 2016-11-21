bb37cd92002ffae93676fedb253f67b6bb11518d
Johan Gall <johan.gall@gmail.com>
2016-11-21 23:56:32 +0900

    moo

---
+++ pearl-metadata/config.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/home/freakhill/.cask/bin
+
+alias e="emacsclient -nw"
+# supossing fasd is there
+alias fe="f -e \"emacsclient -nw\""
---
+++ pearl-metadata/install.sh
@@ -0,0 +1,31 @@
+post_install() {
+    # cask
+    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+    # update
+    pushd "$PEARL_PKGDIR"
+    export PATH="/home/freakhill/.cask/bin:$PATH"
+    cask install # install new emacs packages
+    cask update  # update all packages
+    popd
+    ln -sf "$PEARL_PKGDIR" ~/.emacs.d
+}
+
+pre_update() {
+    echo "nothing yet in pre-update..."
+}
+
+post_update() {
+    cask upgrade # cask itself
+    pushd "$PEARL_PKGDIR"
+    cask install # installs new packages
+    cask update  # updates old
+    popd
+}
+
+pre_remove() {
+    echo "nothing yet in pre-remove..."
+}
+
+post_remove() {
+    echo "nothing yet in post-remove..."
+}
