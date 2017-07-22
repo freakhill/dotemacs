ce4d0321ed172991d2fec2edf709dd3fce3ac924
Johan Gall <johan.gall@gmail.com>
2017-07-22 12:03:29 +0000

    moo

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (nginx-mode package-build shut-up epl git commander f dash s markdown-mode+ polymode yaml-mode zenburn-theme window-numbering web-mode vlf vkill vimish-fold use-package toml-mode tiny smex smart-mode-line-powerline-theme slamhound shell-pop restclient rcirc-color rainbow-delimiters racket-mode racer prodigy org-projectile omnisharp nyan-mode monokai-theme markdown-preview-eww markdown-mode json-reformat irony-eldoc ido-ubiquitous idle-highlight-mode ibuffer-vc htmlize hl-anything highlight helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-cider helm-ag haskell-mode guide-key groovy-mode grizzl git-timemachine git-messenger git-gutter-fringe framesize flycheck-pos-tip flycheck-clojure flycheck-cask flx-ido find-file-in-project fill-column-indicator fancy-narrow exec-path-from-shell evil-terminal-cursor-changer evil-surround evil-numbers evil-multiedit evil-matchit evil-magit evil-lispy evil-god-state evil-exchange elmacro drag-stuff dockerfile-mode discover-my-major dired+ diff-hl deft company-irony color-theme clojure-mode-extra-font-locking clj-refactor clipmon cider-profile cask buffer-move bbyac auto-yasnippet android-mode align-cljlet adoc-mode ace-jump-buffer ac-cider)))
+    (nginx-mode package-build shut-up epl git commander f dash s markdown-mode+ polymode yaml-mode zenburn-theme window-numbering web-mode vlf vkill vimish-fold use-package toml-mode tiny smex smart-mode-line-powerline-theme slamhound shell-pop restclient rcirc-color rainbow-delimiters racket-mode racer prodigy org-projectile omnisharp nyan-mode monokai-theme markdown-preview-eww markdown-mode json-reformat irony-eldoc ido-ubiquitous idle-highlight-mode ibuffer-vc htmlize hl-anything highlight helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-cider helm-ag haskell-mode which-key groovy-mode grizzl git-timemachine git-messenger git-gutter-fringe framesize flycheck-pos-tip flycheck-clojure flycheck-cask flx-ido find-file-in-project fill-column-indicator fancy-narrow exec-path-from-shell evil-terminal-cursor-changer evil-surround evil-numbers evil-multiedit evil-matchit evil-magit evil-lispy evil-god-state evil-exchange elmacro drag-stuff dockerfile-mode discover-my-major dired+ diff-hl deft company-irony color-theme clojure-mode-extra-font-locking clj-refactor clipmon cider-profile cask buffer-move bbyac auto-yasnippet android-mode align-cljlet adoc-mode ace-jump-buffer ac-cider)))
  '(safe-local-variable-values (quote ((c-basic-indent . 4))))
  '(semantic-mode t)
  '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
@@ -588,10 +588,12 @@
   (global-set-key (kbd "C-c C-v") 'aya-expand)
   (evil-define-key 'insert global-map (kbd "C-<tab>") 'aya-open-line))
 
-(defun my-guide-key ()
-  (guide-key-mode t)
-  (setq guide-key/recursive-key-sequence-flag t)
-  (setq guide-key/guide-key-sequence '("C-x" "C-c" "C-r")))
+(defun my-which-key ()
+  (which-key-mode t)
+  (which-key-setup-side-window-bottom)
+  ;; (which-key-setup-side-window-right)
+  ;; (which-key-setup-side-window-right-bottom)
+)
 
 (defun my-android ()
   (cond
@@ -1043,7 +1045,7 @@
    ;; --------------------------------------
    ;; -- configuring ide packages
    ;; --------------------------------------
-   (my-guide-key)
+   (my-which-key)
    (my-ido)
    (my-smex)
    (my-flycheck)
